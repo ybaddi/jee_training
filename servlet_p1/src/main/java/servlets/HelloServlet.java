025b2c393a86f3ed4ba5fce5e49153b8b70fff66
baddi <baddi@baddis-MacBook-Pro.local>
2021-04-01 14:16:03 +0100

    add example2

--- servlet_p1/src/main/java/servlets/HelloServlet.java
+++ servlet_p1/src/main/java/servlets/HelloServlet.java
@@ -9,9 +9,39 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
-		String htmlcode ="<html> <body> <h1> hello baddi </h1> </body> </html>"    ;
+		
+		
+		String val1 = request.getParameter("val1");
+		String val2 = request.getParameter("val2");
+		
+		String htmlcode = "<html> <body> ";
+		
+		if(val1 != null && val2 != null) {
+			
+			int sum = 0;
+			boolean  error = false; 
+			try {
+			sum = Integer.valueOf(val1) + Integer.valueOf(val2);
+			}catch(NumberFormatException e) {
+				htmlcode +="<h1>Error val or val2 is not a number </h1>";	
+				error = true;
+			}
+			
+			if(!error) htmlcode +="<h2> sum of "+ val1  + " and "  + val2 + " is " + sum + " </h1> "   ;
+			
+		}else {
+			
+			 htmlcode +="<h1> val1 and val2 must be not null</h1> "    ;
+		}
+		
+		
+		
+		
+		
+		htmlcode += "</body> </html>" ;
+		
 		
 		
 		PrintWriter out = response.getWriter();
