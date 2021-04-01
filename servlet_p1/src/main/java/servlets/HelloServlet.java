9d0a6dab478307d4bf4b1552b57a85a5066e836d
baddi <baddi@baddis-MacBook-Pro.local>
2021-04-01 15:04:53 +0100

    add example3

--- servlet_p1/src/main/java/servlets/HelloServlet.java
+++ servlet_p1/src/main/java/servlets/HelloServlet.java
@@ -2,10 +2,15 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
 
 public class HelloServlet extends HttpServlet {
 
@@ -16,6 +21,26 @@ public class HelloServlet extends HttpServlet {
 		String val1 = request.getParameter("val1");
 		String val2 = request.getParameter("val2");
 		
+
+		String op = request.getParameter("op");
+		int res=0;
+		
+		
+		
+		
+
+		int day = Integer.valueOf(request.getParameter("day"));
+		int month = Integer.valueOf(request.getParameter("month"));
+		int year = Integer.valueOf(request.getParameter("year"));
+		
+		LocalDate ld = LocalDate.of(year, month, day);
+		
+		int age = Period.between(ld , LocalDate.of(2021, 4, 1) ).getYears();
+		int age_m = Period.between(ld , LocalDate.of(2021, 4, 1) ).getMonths();
+		
+		
+		
+		
 		String htmlcode = "<html> <body> ";
 		
 		if(val1 != null && val2 != null) {
@@ -23,7 +48,21 @@ public class HelloServlet extends HttpServlet {
 			int sum = 0;
 			boolean  error = false; 
 			try {
-			sum = Integer.valueOf(val1) + Integer.valueOf(val2);
+				int val1_int = Integer.valueOf(val1);
+						int val2_int = Integer.valueOf(val2);
+			sum = val1_int + val2_int;
+			
+			
+			
+			switch(op) {
+			case "+": res=val1_int  + val2_int  ;
+			break;
+			case "*" : res = val1_int  * val2_int ;
+			break;
+			}
+			
+			
+			
 			}catch(NumberFormatException e) {
 				htmlcode +="<h1>Error val or val2 is not a number </h1>";	
 				error = true;
@@ -37,6 +76,9 @@ public class HelloServlet extends HttpServlet {
 		}
 		
 		
+		htmlcode +="<h1> mon age est " + age +"  en month " + age_m + " </h1> " ;
+		
+		htmlcode +="<h1> resultat est " + res +" </h1> " ;
 		
 		
 		
