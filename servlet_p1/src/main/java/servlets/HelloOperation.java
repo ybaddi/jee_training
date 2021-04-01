9bea1df00ee6b2aed0b9eece8a4c468bc7a8220d
baddi <baddi@baddis-MacBook-Pro.local>
2021-04-01 15:49:12 +0100

    add example4

---
+++ servlet_p1/src/main/java/servlets/HelloOperation.java
@@ -0,0 +1,100 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class HelloOperation extends HttpServlet {
+
+////	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//		
+//	 handle(request,  response)		;
+//		
+//		
+//		
+//	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		 handle(request,  response)		;
+			
+			
+			
+		}
+	
+	
+	
+
+	private void handle(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		String val1 = request.getParameter("val1");
+		String val2 = request.getParameter("val2");
+		
+
+		String op = request.getParameter("op");
+		int res=0;
+		
+		
+		System.out.println(op);
+			
+		
+		
+		
+		String htmlcode = "<html> <body> ";
+		
+		if(val1 != null && val2 != null) {
+			
+			int sum = 0;
+			boolean  error = false; 
+			try {
+				int val1_int = Integer.valueOf(val1);
+						int val2_int = Integer.valueOf(val2);
+			sum = val1_int + val2_int;
+			
+			
+			
+			switch(op) {
+			case "plus": res=val1_int  + val2_int  ;
+			break;
+			case "mul" : res = val1_int  * val2_int ;
+			break;
+			}
+			
+			
+			
+			}catch(NumberFormatException e) {
+				htmlcode +="<h1>Error val or val2 is not a number </h1>";	
+				error = true;
+			}
+			
+			if(!error) htmlcode +="<h2> sum of "+ val1  + " and "  + val2 + " is " + sum + " </h1> "   ;
+			
+		}else {
+			
+			 htmlcode +="<h1> val1 and val2 must be not null</h1> "    ;
+		}
+		
+		
+		
+		
+		htmlcode +="<h1> resultat est " + res +" </h1> " ;
+		
+		
+		
+		htmlcode += "</body> </html>" ;
+		
+		
+		
+		PrintWriter out = response.getWriter();
+		out.println(htmlcode);
+		
+	}
+}
